ff2805eea8a9a9f78f402f04e2a44fefa4ef1e2e
lastgreatsea <robertsavedlatin@gmail.com>
2018-08-25 12:42:02 -0700

    userInput updated

--- userInput.js
+++ userInput.js
@@ -10,7 +10,38 @@ let botRight = document.getElementById();
 
 function createInput (x) {
     x.innerHTML = userXO;
+    addToArray (x);
     alternateUser();
+}
+
+function addToArray (x) {
+    if (x === topLeft) {
+        this.board[0][0] = userXO;
+    }
+    else if (x === topMid) {
+        this.board[0][1] = userXO;
+    }
+    else if (x === topRight) {
+        this.board[0][2] = userXO;
+    }
+    else if (x === midLeft) {
+        this.board[1][0] = userXO;
+    }
+    else if (x === midMid) {
+        this.board[1][1] = userXO;
+    }
+    else if (x === midRight) {
+        this.board[1][2] = userXO;
+    }
+    else if (x === botLeft) {
+        this.board[2][0] = userXO;
+    }
+    else if (x === botMid) {
+        this.board[2][1] = userXO;
+    }
+    else if (x === botRight) {
+        this.board[2][2] = userXO;
+    }
 }
 
 topLeft.addEventListener('click', createInput(topLeft));
