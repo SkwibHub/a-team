1d3af9b9acf6c0779188334141353cfdb84d80f2
lastgreatsea <robertsavedlatin@gmail.com>
2018-08-25 11:11:46 -0700

    ticTac

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,51 +4,6 @@ let ticTac = {
         this.board = [[null, null, null], [null, null, null], [null, null, null]];
         return this.board;
     },
-    winConditions : function () {
-        if (this.board[0][0] === this.board[0][1] && this.board[0][0] === this.board[0][2]) {
-            if (this.board[0][0] != null) {
-                console.log('winner!');
-            }
-        }
-        else if (this.board[1][0] === this.board[1][1] && this.board[1][0] === this.board[1][2]) {
-            if (this.board[1][0] != null) {
-                console.log('winner');
-            }
-        }
-        else if (this.board[2][0] === this.board[2][1] && this.board[2][0] === this.board[2][2]) {
-            if (this.board[2][0]) {
-                console.log('winner');
-            }
-        }
-        else if (this.board[0][0] === this.board[1][0] && this.board[0][0] === this.board[2][0]) {
-            if (this.board[0][0] != null) {
-                console.log('winner');
-            }
-        }
-        else if (this.board[0][1] === this.board[1][1] && this.board[0][1] === this.board[2][1]) {
-            if (this.board[0][1] != null) {
-                console.log('winner');
-            }
-        }
-        else if (this.board[0][2] === this.board[1][2] && this.board[0][2] === this.board[2][2]) {
-            if (this.board[0][2] != null) {
-                console.log('winner');
-            }
-        }
-        else if (this.board[0][0] === this.board[1][1] && this.board[0][0] === this.board[2][2]) {
-            if (this.board[0][0] != null) {
-                console.log('winner');
-            }
-        }
-        else if (this.board[0][2] === this.board[1][1] && this.board[0][2] === this.board[2][0]) {
-            if (this.board[0][2] !=null) {
-                console.log('winner');
-            }
-        }
-        else {
-            nextTurn();
-        }
-        }
-    }
+}
 
 
