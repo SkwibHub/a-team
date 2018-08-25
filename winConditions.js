ec413b126ec491eb6fd539e938520b1f79d93c58
SkwibHub <eidolongroups@gmail.com>
2018-08-25 12:51:55 -0700

    "Finished testing of function"

--- winConditions.js
+++ winConditions.js
@@ -1,49 +1,49 @@
-function winConditions () {
-    if (this.board[0][0] === this.board[0][1] && this.board[0][0] === this.board[0][2]) {
-        if (this.board[0][0] != null && this.board[0][1] != null && this.board[0][2] != null) {
-            alert(this.board[0][0] + ' is the winner!');
+function winConditions(ttboard) {
+    if (ttboard.board[0][0] === ttboard.board[0][1] && ttboard.board[0][0] === ttboard.board[0][2]) {
+        if (ttboard.board[0][0] != null && ttboard.board[0][1] != null && ttboard.board[0][2] != null) {
+            // alert(ttboard.board[0][0] + ' is the winner!');
             newGame ();
         }
     }
-    else if (this.board[1][0] === this.board[1][1] && this.board[1][0] === this.board[1][2]) {
-        if (this.board[1][0] != null && this.board[1][1] != null && this.board[1][2] != null) {
-            alert(this.board[1][0] + ' is the winner!');
+    else if (ttboard.board[1][0] === ttboard.board[1][1] && ttboard.board[1][0] === ttboard.board[1][2]) {
+        if (ttboard.board[1][0] != null && ttboard.board[1][1] != null && ttboard.board[1][2] != null) {
+            // alert(ttboard.board[1][0] + ' is the winner!');
             newGame ();
         }
     }
-    else if (this.board[2][0] === this.board[2][1] && this.board[2][0] === this.board[2][2]) {
-        if (this.board[2][0] != null && this.board[2][1] != null && this.board[2][2] != null) {
-            alert(this.board[2][0] + ' is the winner!');
+    else if (ttboard.board[2][0] === ttboard.board[2][1] && ttboard.board[2][0] === ttboard.board[2][2]) {
+        if (ttboard.board[2][0] != null && ttboard.board[2][1] != null && ttboard.board[2][2] != null) {
+            // alert(ttboard.board[2][0] + ' is the winner!');
             newGame ();
         }
     }
-    else if (this.board[0][0] === this.board[1][0] && this.board[0][0] === this.board[2][0]) {
-        if (this.board[0][0] != null && this.board[1][0] != null && this.board[2][0] != null) {
-            alert(this.board[0][0] + ' is the winner!');
+    else if (ttboard.board[0][0] === ttboard.board[1][0] && ttboard.board[0][0] === ttboard.board[2][0]) {
+        if (ttboard.board[0][0] != null && ttboard.board[1][0] != null && ttboard.board[2][0] != null) {
+            // alert(ttboard.board[0][0] + ' is the winner!');
             newGame ();
         }
     }
-    else if (this.board[0][1] === this.board[1][1] && this.board[0][1] === this.board[2][1]) {
-        if (this.board[0][1] != null && this.board[1][1] != null && this.board[2][1] != null) {
-            alert(this.board[0][1] + ' is the winner!');
+    else if (ttboard.board[0][1] === ttboard.board[1][1] && ttboard.board[0][1] === ttboard.board[2][1]) {
+        if (ttboard.board[0][1] != null && ttboard.board[1][1] != null && ttboard.board[2][1] != null) {
+            // alert(ttboard.board[0][1] + ' is the winner!');
             newGame ();
         }
     }
-    else if (this.board[0][2] === this.board[1][2] && this.board[0][2] === this.board[2][2]) {
-        if (this.board[0][2] != null && this.board[1][2] != null && this.board[2][2] != null) {
-            alert(this.board[0][2] + ' is the winner!');
+    else if (ttboard.board[0][2] === ttboard.board[1][2] && ttboard.board[0][2] === ttboard.board[2][2]) {
+        if (ttboard.board[0][2] != null && ttboard.board[1][2] != null && ttboard.board[2][2] != null) {
+            // alert(ttboard.board[0][2] + ' is the winner!');
             newGame ();
         }
     }
-    else if (this.board[0][0] === this.board[1][1] && this.board[0][0] === this.board[2][2]) {
-        if (this.board[0][0] != null && this.board[1][1] != null && this.board[2][2] != null) {
-            alert(this.board[0][0] + ' is the winner!');
+    else if (ttboard.board[0][0] === ttboard.board[1][1] && ttboard.board[0][0] === ttboard.board[2][2]) {
+        if (ttboard.board[0][0] != null && ttboard.board[1][1] != null && ttboard.board[2][2] != null) {
+            // alert(ttboard.board[0][0] + ' is the winner!');
             newGame ();
         }
     }
-    else if (this.board[0][2] === this.board[1][1] && this.board[0][2] === this.board[2][0]) {
-        if (this.board[0][2] != null && this.board[1][1] != null && this.board[2][0] != null) {
-            alert(this.board[0][2] + ' is the winner!');
+    else if (ttboard.board[0][2] === ttboard.board[1][1] && ttboard.board[0][2] === ttboard.board[2][0]) {
+        if (ttboard.board[0][2] != null && ttboard.board[1][1] != null && ttboard.board[2][0] != null) {
+            // alert(ttboard.board[0][2] + ' is the winner!');
             newGame ();
         }
     }
@@ -51,3 +51,24 @@ function winConditions () {
         return nextTurn();
     }
     }
+
+// BELOW IS FOR TESTING. BELOW FUNCTIONS WILL NEED TO UPDATE
+
+function newGame() {
+    console.log("WINNER!");
+}
+
+function nextTurn() {
+    console.log("No Winner Yet");
+}
+
+// ---------------------------------------
+// BELOW IS USED ONLY FOR TESTING
+
+let ttBoard = {
+    board: [["X","O","X"],[null,"X","O"],["X",null,null]]
+}
+
+winConditions(ttBoard);
+
+
