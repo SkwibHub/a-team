e5b449ffd1598e40c4051fbbc05ce614ee8690f5
lastgreatsea <robertsavedlatin@gmail.com>
2018-08-25 10:00:06 -0700

    board and clear

---
+++ tic-tac-toe.js
@@ -0,0 +1,8 @@
+let ticTac = {
+    board : [[null, null, null], [null, null, null], [null, null, null]],
+    clear : function () {
+        this.board = [[null, null, null], [null, null, null], [null, null, null]];
+    },
+}
+
+console.log(ticTac);
